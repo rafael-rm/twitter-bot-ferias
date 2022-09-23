b448b663b1e5aaec201fbcdb0e92fa1ff3617489
rafael-rm <rafael.rmo@outlook.com>
2022-09-23 17:46:45 -0300

    Adicionado suporte ao Repl.it

--- main.py
+++ main.py
@@ -6,25 +6,33 @@ from time import sleep
 from discord_webhook import DiscordWebhook
 import utils.functions as functions
 
+# Carregando as variáveis de ambiente
 dotenv.load_dotenv(dotenv.find_dotenv())
 access_token_dot = os.getenv("ACESS_TOKEN")
 access_token_secret_dot = os.getenv("ACESS_TOKEN_SECRET")
 consumer_key_dot = os.getenv("CONSUMER_KEY")
 consumer_secret_dot = os.getenv("CONSUMER_SECRET")
+webhook_url_dot = os.getenv("WEBHOOK_URL")
+data_ferias_dot = os.getenv("DATA_FERIAS")
+
+# USAR NO REPL.IT
+# Carregando as variáveis de ambiente
+# access_token_dot = os.environ['ACESS_TOKEN']
+# access_token_secret_dot = os.environ['ACESS_TOKEN_SECRET']
+# consumer_key_dot = os.environ['CONSUMER_KEY']
+# consumer_secret_dot = os.environ['CONSUMER_SECRET']
+# webhook_url_dot = os.environ['WEBHOOK_URL']
+# data_ferias_dot  = os.environ['DATA_FERIAS'] 
 
 access_token_dot = str(access_token_dot)
 access_token_secret_dot = str(access_token_secret_dot)
 consumer_key_dot = str(consumer_key_dot)
 consumer_secret_dot = str(consumer_secret_dot)
 
-data_ferias_dot = os.getenv("DATA_FERIAS")
 data_ferias_temp = datetime.strptime(data_ferias_dot, "%d/%m/%Y")
 dia_ferias_dot = data_ferias_temp.day
 mes_ferias_dot = data_ferias_temp.month
 ano_ferias_dot = data_ferias_temp.year
-
-
-webhook_url_dot = os.getenv("WEBHOOK_URL")
 
 auth = tweepy.OAuthHandler(consumer_key_dot, consumer_secret_dot)
 auth.set_access_token(access_token_dot, access_token_secret_dot)
@@ -70,7 +78,7 @@ def main():
             print("Um novo post acaba de ser realizado.")
             webhook = DiscordWebhook(url=webhook_url_dot, content='Um novo post acaba de ser realizado.')
             webhook.execute()
-        sleep(100)
+        sleep(600)
 
 if __name__ == "__main__":
     main()
